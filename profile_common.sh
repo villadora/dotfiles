e99528cacc2442c7a37e43492870a151d3672b50
villadora <jky239@gmail.com>
2016-04-25 14:01:01 +0800

    modified

--- profile_common.sh
+++ profile_common.sh
@@ -3,7 +3,16 @@
 PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/sbin:/usr/sbin:/usr/local/git/bin
 
 # add commands to PATH
-PATH=$PATH:$HOME/.dotfiles/bin:$HOME/bin
+PATH=$HOME/bin:$PATH:$HOME/.dotfiles/bin
+
+# Android Home
+export ANDROID_HOME=/usr/local/Cellar/android-sdk/24.3.4
+
+# GOPATH
+export GOPATH=$HOME/dev/gopath
+
+# Homebrew
+export HOMEBREW_GITHUB_API_TOKEN="14dfae85812adbcffe896ce6ad5dbb5c25360909"
 
 # set editor to emacs style
 EDITOR=emacs
