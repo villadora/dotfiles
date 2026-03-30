b5f81b9bc7fe5aeeca9b01d5e03da6d5ed7a6dee
villadora <jky239@gmail.com>
2026-03-30 10:37:26 +0800

    upgrade

--- profile_common.sh
+++ profile_common.sh
@@ -11,9 +11,6 @@ PATH=$PATH:/opt/homebrew/bin
 # add pipx commands
 PATH=$PATH:$HOME/.local/bin
 
-# Homebrew
-export HOMEBREW_GITHUB_API_TOKEN="be804c756700b3089b58f570c149bf9e0d2726ff"
-
 # set editor to emacs style
 EDITOR=emacs
 
@@ -31,24 +28,11 @@ fi
 readonly OS
 
 
-# pyenv
-export PYENV_ROOT="/usr/local/var/pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-
-if which pyenv > /dev/null; then
-    eval "$(pyenv init --path)"
-fi
-
 # jenv
 export PATH="$HOME/.jenv/bin:$PATH"
 eval "$(jenv init -)"
 
 
-
-# nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 # virtual env
 #source ~/.dotfiles/virtualenv-autoenv.sh
